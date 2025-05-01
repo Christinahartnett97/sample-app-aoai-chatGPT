7e206f187dd36d84606f71f88caa8154b9a19a8e
Christinahartnett97 <Christina.hartnett@chsli.org>
2025-05-01 17:14:37 -0400

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -12,7 +12,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import styles from './Chat.module.css'
-import Contoso from '../../assets/Contoso.svg'
+import Contoso from '../../assets/Catholic.svg'
 import { XSSAllowTags } from '../../constants/sanatizeAllowables'
 
 import {
@@ -117,9 +117,7 @@ const Chat = () => {
   }, [appStateContext?.state.chatHistoryLoadingState])
 
   const getUserInfoList = async () => {
-    if (!AUTH_ENABLED) {
-      setShowAuthMessage(false)
-      return
+        setShowAuthMessage(false);
     }
     const userInfoList = await getUserInfo()
     if (userInfoList.length === 0 && window.location.hostname !== '127.0.0.1') {
